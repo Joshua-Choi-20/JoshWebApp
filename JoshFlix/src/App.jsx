1df8397a31e5b320188d3d494ecfe98d0f9a23c5
Joshua-Choi-20 <joshuachoi679@gmail.com>
2025-01-26 20:01:25 -0500

    changes to web

--- JoshFlix/src/App.jsx
+++ JoshFlix/src/App.jsx
@@ -3,17 +3,13 @@ import "../src/App.css"
 export default function App() {
   return (
     <>
-      <div className="bg-young-josh-mobile bg-cover md:bg-young-josh h-screen w-full ">
+      <div className="w-full h-full bg-young-josh-mobile bg-cover md:bg-young-josh">
         {/* Horizontal Flex */}
         <div className="flex items-center">
           {/* Sidebar */}
-          <div class="flex flex-col justify-evenly items-center w-1/12 h-screen bg-gray-800">
-            <button>
-              linkedin
-            </button>
-            <button>
-              sad
-            </button>
+          <div class="w-1/12 h-screen ">
+         
+           
           </div>
 
           {/* Main Page */}
@@ -28,22 +24,23 @@ export default function App() {
               </h1>
 
               <h2 className="text-xl md:text-2xl font-bold ">
-                #1 in Websites You Need to Visit!
+                #1 in Websites You Need to Visit
               </h2>
 
               <h3 className="text-sm md:text-2x1 w-3/5 text-white">
                 <p>Software Engineer - First Generation Korean - Youngest of 4</p>
-                <p>Loves Chess, Calisthenics, Malatang Spice, and Live Music!</p>
-                <p>Currently New York City</p>
-                <p>Previously Honolulu, Hawaii - College Park, MD - Severna Park, MD</p>
+                <div></div>
+                <p>I love games 🎲, calisthenics 🤸, malatang spice 🌶️, and live music 🎶</p>
+                <div></div>
+                <p>Check out some of my favorite "shows" below!</p>
               </h3>
 
-              <div class="flex w-1/6 h-8 justify-between">
-                <button class="w-min-fit w-1/2 h-full bg-yellow-400">
-                  Resume
+              <div class="text-sm flex w-1/2 h-8 md:justify-start">
+                <button class="w-min-fit w-1/2 h-full bg-yellow-400" >
+                  <a href='https://www.linkedin.com/in/joshua--choi/'>Resume</a>
                 </button>
                 <button class="w-min-fit w-1/2 h-full bg-yellow-700">
-                  More Info
+                <a href='https://www.linkedin.com/in/joshua--choi/'>LinkedIn</a>
                 </button>
               </div>
             </div>
@@ -52,26 +49,32 @@ export default function App() {
               Trending Now
             </div>
 
-            <div class="grid w-10/12 mx-10 container h-1/6 gap-3 grid-cols-2 md:grid-cols-4 mb-10">
-              <div class="w-full h-full item bg-store-josh"></div>
-              <div class="w-full  h-full item bg-sunglass-josh"></div>
-              <div class="w-full  h-full item bg-college-josh"></div>
-              <div class="w-full  h-full item bg-beach-josh"></div>
-              {/* <div class="item bg-birthday-josh"></div> */}
-            </div>
-            {/* <div class="w-full h-1/6 bg-yellow-950 text-center group" >
-              <div class="flex p-5 justify-evenly items-center h-full item transform group-hover:-translate-x-1/4 group">
-                <div class="bg-green-200 flex-auto h-full item transform group-hover:translate-x-1/4">1</div>
-                <div class="bg-red-100 flex-auto h-full">2</div>
-                <div class="bg-yellow-300 flex-auto h-full">3</div>
-                <div class="bg-slate-500 flex-auto h-full">4</div>
-                <div class="bg-white flex-auto h-full">5</div>
-                
-                <div class="bg-green-200 flex-auto h-full">6</div>
-    
-                <div class="bg-red-100 flex-auto h-full">7</div>
+            <div class="text-sm grid w-10/12 mx-10 container h-1/6 gap-3 grid-cols-3  md:grid-cols-6 mb-10 sm:grid-cols-6">
+              <div class="col-span-1 item flex justify-center items-center bg-store-josh bg-cover ">
+                <p>Kim's Convenience</p>
               </div>
-            </div> */}
+              <div class="col-span-1  flex justify-center items-center item bg-sunglass-josh bg-cover">
+                <a href="https://www.op.gg/summoners/na/heaven-679">Arcane</a>
+              </div>
+              <div class="col-span-1  flex justify-center items-center item bg-college-josh bg-cover">
+                <p>
+                  <a href="https://www.instagram.com/sunnoshirt/">Physical 100</a> 
+                </p>
+              </div>
+              <div class="col-span-1  flex justify-center items-center item bg-beach-josh bg-cover">
+                <a href="https://www.chess.com/member/sunyongc">Queen's Gambit</a>
+              </div>
+              <div class="col-span-1  flex justify-center items-center item bg-birthday-josh bg-cover">
+                <a href="https://open.spotify.com/playlist/37i9dQZF1FoAVPBJ8rfR3B?si=a2dbd71bafa841d6">moosic</a>
+                <p>
+
+                </p>
+              </div>
+              <div class="col-span-1  flex justify-center items-center item bg-birthday-josh bg-cover">
+                <a href="https://beliapp.co/app/heaven679">Korea No.1</a>
+              </div>
+            </div>
+           
 
           </div>
 
