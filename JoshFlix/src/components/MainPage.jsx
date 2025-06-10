040e80d868e151fb3435497ffa3f057f59120d43
Joshua Choi <joshuachoi679@gmail.com>
2025-06-10 12:28:51 -0400

    modeling page added

--- JoshFlix/src/components/MainPage.jsx
+++ JoshFlix/src/components/MainPage.jsx
@@ -1,47 +1,51 @@
-
-import Carousel  from "./Carousel.jsx";
+import Carousel from "./Carousel.jsx";
 
 export function ContentText({ textTop, heading, subheading, bio }) {
-    return (<>
-        <p class="text-3x1 md:text-6xl ">{textTop}</p>
-        <h1 class="text-5x1 md:text-9xl font-extrabold">{heading}</h1>
-        <h2 className="text-xl md:text-4xl font-bold ">{subheading}</h2>
-        <h3 className="text-sm md:text-4xl text-white">
-        {bio.split('\n').map((line, index) => (
-                <p key={index}>{line}</p>
-            ))}</h3>
-    </>)
-
+  return (
+    <>
+      <p class="text-3x1 md:text-6xl ">{textTop}</p>
+      <h1 class="text-5x1 md:text-9xl font-extrabold">{heading}</h1>
+      <h2 className="text-xl md:text-4xl font-bold ">{subheading}</h2>
+      <h3 className="text-sm md:text-4xl text-white">
+        {bio.split("\n").map((line, index) => (
+          <p key={index}>{line}</p>
+        ))}
+      </h3>
+    </>
+  );
 }
 
-
-export function Content({ textTop, heading, subheading, bio, display }) {
-    return (
-        <div className="flex flex-col items-center space-y-4 text-center">
-                <ContentText textTop={textTop} heading={heading} subheading={subheading} bio={bio} />
-        </div>
-    );
+export function Content({ textTop, heading, subheading, bio}) {
+  return (
+    <div className="flex flex-col items-center space-y-4 text-center">
+      <ContentText
+        textTop={textTop}
+        heading={heading}
+        subheading={subheading}
+        bio={bio}
+      />
+    </div>
+  );
 }
-
-
 
 export default function MainPage() {
-    return (
-        <div className="bg-cover w-full h-full flex flex-col bg-gradient-to-r from-black 
-        space-y-12">
-            <div className="pt-[12.5vh] h-5/8 flex flex-col">
-                <Content
-                textTop={'A 선용 Original'}
-                heading={'Josh\'s Website'}
-                subheading={'Design Engineer'}
-                bio={'I love nature 🎲, malatang spice 🌶️, and live music 🎶'}
-                />
-            </div>
+  return (
+    <div
+      className="bg-cover w-full h-full flex flex-col 
+        space-y-12"
+    >
+      <div className="pt-[12.5vh] h-5/8 flex flex-col">
+        <Content
+          textTop={"A 선용 Original"}
+          heading={"Josh's Website"}
+          subheading={"Design Engineer"}
+          bio={"I love nature 🎲, malatang spice 🌶️, and live music 🎶"}
+        />
+      </div>
 
-            <div className = "h-1/8 flex justify-center">
-            <Carousel/>
-            </div>
-            
-        </div>
-    )
+      <div className="h-1/8 flex justify-center">
+        <Carousel />
+      </div>
+    </div>
+  );
 }
--- JoshFlix/src/components/Modeling.jsx
+++ JoshFlix/src/components/Modeling.jsx
@@ -1,37 +1,52 @@
 export function Photos() {
-    return (
-        <div className="w-1/2 h-full px-8 py-4 outline-dashed outline-blue-900 flex flex-col items-center">
-            <div className="w-full h-1/2 outline-dashed outline-yellow-600"></div>
-            <div className="flex w-full h-1/6">
-                   <div className="w-1/3 h-full outline-dashed outline-yellow-600"></div>
-                    <div className="w-1/3 h-full outline-dashed flex outline-yellow-600">
-                        <div className="w-1/2 h-full outline-dashed outline-purple-700 bg-white"></div>
-                        <div className="w-1/2 h-full outline-dashed outline-purple-700 flex flex-col">
-                            <div className="h-1/2 outline-double outline-purple-400 bg-blue-500"></div>
-                             <div className="h-1/2 outline-double outline-purple-400 bg-red-400"></div>
-                        </div>
-                    </div>
-                    <div className="w-1/3 h-full outline-dashed outline-yellow-600"></div>
-            </div>
+  return (
+    <div className="w-1/2 h-full outline-dashed outline-blue-900 p-4 flex flex-col items-center">
+      <div className="w-full h-2/3 outline-dashed outline-yellow-600">
+      video </div>
+      <div className="flex w-full h-1/3 m-4 gap-4">
+        <div className="w-1/3 h-full outline-dashed outline-yellow-600">
+        image
         </div>
-    )
+        <div className="w-1/3 h-full outline-dashed flex outline-yellow-600 gap-2">
+          <div className="w-1/2 h-full outline-dashed outline-purple-700 bg-white"> lli image</div>
+          <div className="w-1/2 h-full outline-dashed outline-purple-700 flex gap-2 flex-col">
+            <div className="h-1/2 w-full outline-dashed outline-purple-400 bg-blue-500">tiny</div>
+            <div className="h-1/2 w-full outline-dashed outline-purple-400 bg-red-400">tiny</div>
+          </div>
+        </div>
+        <div className="w-1/3 h-full outline-dashed outline-yellow-600">image</div>
+      </div>
+    </div>
+  );
 }
 
 export default function Modeling() {
-    return (
-        <div className="w-full h-full flex bg-gradient-to-r from-black">
-            <div className="w-1/2 h-full flex flex-col outline-dashed outline-orange-800 py-4 px-8">
-                <h1 className = 'text-3x1 md:text-6xl font-extrabold'>KIMCHI X SUNDAE SCHOOL</h1>
-                <br/>
-                <p className="text-5x1 md:text-2xl">I was one of four models in a commercial for Korean-smokewear brand “Sundae School”. I had the amazing opportunity to work directly with Korean-American drag queen Kim Chi, producer Ellie Rha and creative directors Corrine Ang and Hee Eun Chung</p>
-                <br/>
-                <p className="text-5xl md:text-2xl">I am incredibly thankful to Sundae School with connecting me with so many talented people</p>
+  return (
+    <div className="w-full h-3/4 flex bg-gradient-to-r from-black">
+      <div className="w-1/2 h-full flex flex-col outline-dashed outline-orange-800 p-12">
+        <h1 className="text-3x1 md:text-6xl font-extrabold">
+          KIMCHI X SUNDAE SCHOOL
+        </h1>
+        <br/>
+        <p className="text-5x1 md:text-2xl">
+          I was one of four models in a commercial for Korean-smokewear brand
+          “Sundae School”. I had the amazing opportunity to work directly with
+          Korean-American drag queen Kim Chi, producer Ellie Rha and creative
+          directors Corrine Ang and Hee Eun Chung
+        </p>
+        <br/>
+        <p className="text-5xl md:text-2xl">
+          I am incredibly thankful to Sundae School with connecting me with so
+          many talented people
+        </p>
+        <br/>
+        <div className="flex h-[4vh] w-1/3 items-start justify-between gap-10">
+            <div className="outline-dashed outline-yellow-300 w-1/2 h-full">
             </div>
-            <Photos/>
-            {/* <div className="w-1/2 h-full outline-dotted outline-green-700 bg-white">
-                <Photos/>
-            </div> */}
-            
+             <div className="outline-dashed outline-yellow-300 w-1/2 h-full"></div>
         </div>
-    )
+      </div>
+      <Photos />
+    </div>
+  );
 }
